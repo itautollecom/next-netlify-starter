b8e6d471700cbc8039bdedac2d12c5bbf9b81480
kantaree <kantareeli@gmail.com>
2023-11-16 13:48:52 +0200

    add autolle

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter Test Hello!</title>
+        <title>Autolle.com api documents</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        {/* link to autolle.com */}
+        <a href="https://autolle.com" >https://autolle.com</a>
       </main>
 
       <Footer />
