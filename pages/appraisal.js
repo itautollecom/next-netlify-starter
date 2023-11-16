b0dca59550f5f0f0a38dada067cf5f4072ae3d8f
kantaree <kantareeli@gmail.com>
2023-11-16 15:02:24 +0200

    remove swagger

--- pages/appraisal.js
+++ pages/appraisal.js
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import SwaggerUI from 'swagger-ui-react';
-import 'swagger-ui-react/swagger-ui.css';
 
 export default function Home() {
   return (
@@ -17,7 +15,6 @@ export default function Home() {
         <br />
         https://petstore.swagger.io/v2/swagger.json
         <br />
-        {/* <SwaggerUI url="https://open-api.netlify.com/swagger.json" /> */}
       </main>
 
     </div>
