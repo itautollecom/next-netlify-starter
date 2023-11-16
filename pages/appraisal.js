0f9f046f6fb7e512d7dba9ca094766b5a7d26089
kantaree <kantareeli@gmail.com>
2023-11-16 14:35:09 +0200

    add test appraisal

---
+++ pages/appraisal.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Autolle.com api documents</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        Hello world
+      </main>
+
+    </div>
+  )
+}
